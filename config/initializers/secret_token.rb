8556214f9aa7e563d7881dd30b2b5ecd33626d6d
WesleySmits <wesleysmits@gmail.com>
2013-09-30 15:37:43 +0200

    Commit full rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CaptainGreed::Application.config.secret_key_base = '64cdd5776021b31ba86ff39935d99624b86f71eacc91956bfcfaf0db0118a39927cd36c83c7ef0093b1f68496211e84a0afd1929b9a31c9cd85dbde1f6c8d4a6'
